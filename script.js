867471125d6dbbd771b1dca5787f79533731c580
Praveen Dubey <dubeypraveen1506@gmail.com>
2022-11-12 19:45:47 +0530

    Amazon Clone

---
+++ script.js
@@ -0,0 +1,37 @@
+// Slide show using the arrow
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+
+
+// Slide show directly after every 4 seconds
+
+var myIndex = 0;
+slide();
+
+function slide() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(slide, 4000); // Change image every 4 seconds
+}
